cadf455226c613a68cbbcb3eed107b5e542f12e1
wt032320 <1476081727@qq.com>
2021-11-16 21:05:23 +0800

    fix: 绿点不出现

--- src/models/adminTable.js
+++ src/models/adminTable.js
@@ -1,6 +1,6 @@
-const inspirecloud = require("@byteinspire/api");
+// const inspirecloud = require("@byteinspire/api");
 
-// 管理员信息表
-const adminTable = inspirecloud.db.table('admin');
+// // 管理员信息表
+// const adminTable = inspirecloud.db.table('admin');
 
-module.exports = adminTable;
+// module.exports = adminTable;
--- src/services/adminService.js
+++ src/services/adminService.js
@@ -1,4 +1,4 @@
-const inspirecloud = require("@byteinspire/api");
+// const inspirecloud = require("@byteinspire/api");
 
-const adminTable = require('../models/adminTable');
+// const adminTable = require('../models/adminTable');
 
